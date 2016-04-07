1b2b59923a90e11a6ecd4059cccfecbecefe058c
kenma95 <kenma95@gmail.com>
2016-04-06 22:32:12 -0400

    clean unused files

--- marsh_test.py
+++
@@ -1,6 +0,0 @@
-class ItemSchema(ma.Schema):
-	class Metat:
-		#Fields to expose
-		fields = ('item_id','item_name','price','address')
-item_schema = ItemSchema()
-item_schema = ItemSchema(many=True)
